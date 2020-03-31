34e4993953b1a6507d4155e839a3afb6c434f738
Prabhakaran Kasinathan <prabhakaran1989@gmail.com>
2020-03-31 23:29:45 +0200

    added docker start/stop, requierments duplicate file deleted

---
+++ docker-start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# load .env vars
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+docker start ${DOCKER_COVID_MYSQL} 
+docker start ${DOCKER_COVID_ADMIN}
