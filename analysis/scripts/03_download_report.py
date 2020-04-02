829dd34c1c6574da95b9f5487f6695da841f58db
nuffer <nuffer@magicleap.com>
2020-04-02 21:02:12 +0200

    nothing

--- analysis/scripts/03_download_report.py
+++ analysis/scripts/03_download_report.py
@@ -2,8 +2,8 @@ from analysis.utils.download_report import download_hours_frame
 
 
 def run_selected_frame():
-    from_time = '2020-03-31T06'  # warning UTC timezone
-    to_time = '2020-03-31T16'
+    from_time = '2020-04-02T17'  # warning UTC timezone
+    to_time = '2020-04-02T20'
     download_hours_frame(from_time, to_time, 10)
 
 
